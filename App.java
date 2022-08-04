c619796bdbd1c8633bd31320e8dfe57b24eea1d7
Raf Treadway <rafael401@revature.net>
2022-08-04 17:54:25 -0400

    details for InputUtil.java

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 package com.revature;
 
 import com.revature.consoleviews.MainMenu;
-
+import com.revature.util.*;
 /**
  * Grocery List Target Functionality
  *  - register new user
@@ -17,9 +17,14 @@ import com.revature.consoleviews.MainMenu;
 public class App 
 {
     public static void main( String[] args)
+    // {
+    //     // Create Mainmenu object, then call the view() to start the program
+    //     MainMenu mainmenu = new MainMenu();
+    //     mainmenu.view();
+    // }
     {
-        // Create Mainmenu object, then call the view() to start the program
-        MainMenu mainmenu = new MainMenu();
-        mainmenu.view();
+        InputUtil input = new InputUtil();
+        Integer num = input.retrieveInt("Enter a number plx: ");
+        System.out.println(num);
     }
 }
--- util/InputUtil.java
+++ util/InputUtil.java
@@ -2,25 +2,47 @@ package com.revature.util;
 
 import java.util.Scanner;
 
-public class InputUtil {
+/*
+ * Utility class made to handle user input AND menu messaging for the console app
+ */
 
+public class InputUtil {
+    // Declare scanner input object
     Scanner scanner = new Scanner(System.in);
     
     public String retrieveString(String msg){
+        // Method to print a message to the user and prompt for a String input
+        // Returns the input to be used in other parts of the program
         System.out.print(msg);
         return scanner.nextLine();
     }
 
     public Integer retrieveInt(String msg){
+        // Method to print a message to the user and prompt for a Integer input
+        // Checks for NumberFormatException to handle incorrect non-Integer input
+        // Returns the input to be used in other parts of the program
+        
+        // First some control vars
+        //  num: will contain the value of the scanner input, if it's an Integer.
+        //          it's declared outside of the loop so it may be returned when the loop breaks (SCOPE)
+        //  valid: the loop control variable
         Integer num = 0;
-        System.out.println(msg);
-        try{
-            num = Integer.parseInt(scanner.nextLine());
-            
-        }catch(NumberFormatException nfe){
-            System.out.println("Input is not an integer, try again.");
+        Boolean valid = false;
+        // NOTE the "not" operator (!) which effectively makes 'valid' true
+        while(!valid){
+            System.out.println(msg);
+            // try-catch to account for non-Integer inputs.  When the exception is caught, 
+            //      the program loops back to the input message rather than crash
+            try{
+                num = Integer.parseInt(scanner.nextLine());
+                // If a valid Integer was input, close the loop
+                valid = true;
+                break;
+            }catch(NumberFormatException nfe){
+                System.out.println("Input is not an integer, try again.");
+            }
         }
+        // When the loop is closed, return the valid Integer
         return num;
     }
-    
 }
